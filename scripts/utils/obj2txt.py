a869ad5020fc08699cb458afbeac61a2a556b7d1
SEMBLANET Tom <tomsemblanet@hotmail.fr>
2021-04-21 14:15:43 +0200

    -

--- scripts/utils/obj2txt.py
+++ scripts/utils/obj2txt.py
@@ -2,7 +2,8 @@ import sys
 
 import pickle as pkl
 import pykep as pk
-import pygmo as pg 
+import pygmo as pg
+import numpy as np 
 
 if __name__ == '__main__':
 
@@ -15,4 +16,6 @@ if __name__ == '__main__':
 	# Extraction of the decision vector
 	x = results['population'].get_x()[0]
 
-	print(x)
+	# Writing of the decision vector into a file text
+	txt_file_nm = '~/Asteroid-Retrieval-Mission/local/txt_files/' + str(round(x[0], 5)) + '.txt'
+	np.savetxt(txt_file_nm, x, fmt='%.2f')

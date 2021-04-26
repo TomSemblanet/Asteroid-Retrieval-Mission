d6fc12567fd15d3545ff53e2340acaf209dff9b6
SEMBLANET Tom <tomsemblanet@hotmail.fr>
2021-04-26 17:05:59 +0200

    Added the NEA -> Earth transfer targeting Moon

--- scripts/missions/NEA_Earth/NEA_Earth_Moon_LGA_SC.py
+++ scripts/missions/NEA_Earth/NEA_Earth_Moon_LGA_SC.py
@@ -20,7 +20,7 @@ from datetime import date
 from mpi4py import MPI
 
 from data import constants as cst
-from scripts.udp.NEA_Earth_UDP import NEA2Earth
+from scripts.udp.NEA_Earth_UDP_Moon_LGA import NEA2Earth
 from scripts.utils.post_process import post_process
 from scripts.missions.NEA_Earth.NEA_Earth_Initial_Guess import initial_guess
 from scripts.utils import load_sqp, load_kernels, load_bodies

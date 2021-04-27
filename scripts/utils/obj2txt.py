991c62cc7486c18af47630fd0044e448fecd4cb5
SEMBLANET Tom <tomsemblanet@hotmail.fr>
2021-04-27 13:58:02 +0200

    Modifications on obj2txt

--- scripts/utils/obj2txt.py
+++ scripts/utils/obj2txt.py
@@ -1,5 +1,6 @@
 import sys
 
+import getpass
 import pickle as pkl
 import pykep as pk
 import pygmo as pg
@@ -17,5 +18,8 @@ if __name__ == '__main__':
 	x = results['population'].get_x()[0]
 
 	# Writing of the decision vector into a file text
-	txt_file_nm = '/home/cesure/t.semblanet/Asteroid-Retrieval-Mission/local/txt_files/' + str(int(x[0])) + '.txt'
+	if 'semblanet' in getpass.getuser():
+		txt_file_nm = '/home/cesure/t.semblanet/Asteroid-Retrieval-Mission/local/txt_files/' + str(int(x[0])) + '.txt'
+	else:
+		txt_file_nm = '/home/dcas/yv.gary/SEMBLANET/Asteroid-Retrieval-Mission/local/txt_files/' + str(int(x[0])) + '.txt'
 	np.savetxt(txt_file_nm, x, fmt='%.50f')

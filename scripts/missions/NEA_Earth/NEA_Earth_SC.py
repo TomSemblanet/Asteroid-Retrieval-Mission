5405129410ab7a04837ff871ac2319ed108dc48e
SEMBLANET Tom <tomsemblanet@hotmail.fr>
2021-04-29 08:35:41 +0200

    Augmented the maximum dV at Earth arrival

--- scripts/missions/NEA_Earth/NEA_Earth_SC.py
+++ scripts/missions/NEA_Earth/NEA_Earth_SC.py
@@ -78,7 +78,7 @@ lw_upp = pk.epoch_from_string(str(year) + '-12-31 23:59:59')
 # 3 - Time of flight
 # ------------------
 tof_low = cst.YEAR2DAY * 0.70
-tof_upp = cst.YEAR2DAY * 5.00
+tof_upp = cst.YEAR2DAY * 4.00
 
 # 4 - Spacecraft
 # --------------
@@ -88,7 +88,7 @@ Isp = 3000
 
 # 5 - Earth arrival 
 # -----------------
-vinf_max = 2.5e3
+vinf_max = 3.5e3
 
 # 6 - Optimization algorithm
 # --------------------------

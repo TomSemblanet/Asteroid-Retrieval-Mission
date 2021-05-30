7089ebbfcb8779aa073e9756406649ed2bc96699
SEMBLANET Tom <tomsemblanet@hotmail.fr>
2021-05-30 19:31:09 +0200

    -

--- scripts/earth_departure/Apogee_Raising_Error_Matrix.py
+++ scripts/earth_departure/Apogee_Raising_Error_Matrix.py
@@ -21,7 +21,8 @@ theta_list = np.sort(theta_list)
 error_matrix = np.zeros((len(theta_list), 2000, 2))
 
 for k, theta in enumerate(theta_list):
-	sub_matrix = np.loadtxt('/home/dcas/yv.gary/SEMBLANET/Asteroid-Retrieval-Mission/local/error_matrices/'+str(theta))
+	with open('/home/dcas/yv.gary/SEMBLANET/Asteroid-Retrieval-Mission/local/error_matrices/'+str(theta), 'rb') as file:
+		sub_matrix = pickle.load(file)
 
 	error_matrices[k, :, 0] = sub_matrix[:, 0]
 	error_matrices[k, :, 1] = sub_matrix[:, 1]

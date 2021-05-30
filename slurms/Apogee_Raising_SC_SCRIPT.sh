ca951d8b2af6999e4dec046f480afadbfc75eab2
SEMBLANET Tom <tomsemblanet@hotmail.fr>
2021-05-30 17:19:23 +0200

    Deep search for apogee raising in CR3BP added

--- slurms/Apogee_Raising_SC_SCRIPT.sh
+++ slurms/Apogee_Raising_SC_SCRIPT.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for ((i=1;i<360;i+=1)); do --export=theta=${i} sbatch Apogee_Raising_SC.slurm.sh ; done
+for ((i=1;i<360;i+=1)); do sbatch --export=theta=${i} Apogee_Raising_SC.slurm.sh ; done
